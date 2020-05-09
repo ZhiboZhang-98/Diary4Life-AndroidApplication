22924181d211931c9509f35b997255d18b4ad487
Zhibo Zhang <61286059+ZhiboZhang-98@users.noreply.github.com>
2020-05-09 19:56:47 -0400

    Added Application Login Screen
    Created LoginActivity and its layout.

--- Diary4Life/app/src/main/java/com/test/diary4life/Activities/LoginActivity.java
+++ Diary4Life/app/src/main/java/com/test/diary4life/Activities/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.test.diary4life;
+package com.test.diary4life.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.test.diary4life.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
