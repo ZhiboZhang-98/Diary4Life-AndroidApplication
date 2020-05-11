6b73933112fc9c597446bf995d8db40f41d61f8f
Zhibo Zhang <61286059+ZhiboZhang-98@users.noreply.github.com>
2020-05-11 15:07:43 -0400

    Added Application User Registration Screen
    Created RegisterActivity and its layout.

---
+++ Diary4Life/app/src/main/java/com/test/diary4life/Activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.test.diary4life.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.test.diary4life.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
