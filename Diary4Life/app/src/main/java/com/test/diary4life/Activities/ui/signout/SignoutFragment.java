d79b9a4919947018172beaa78393f4c3bd3443fe
Zhibo Zhang <61286059+ZhiboZhang-98@users.noreply.github.com>
2020-05-16 19:11:50 -0400

    Added Application User Sign out Option
    Created SignoutFragment and its corresponding layout.

---
+++ Diary4Life/app/src/main/java/com/test/diary4life/Activities/ui/signout/SignoutFragment.java
@@ -0,0 +1,38 @@
+package com.test.diary4life.Activities.ui.signout;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.test.diary4life.R;
+
+public class SignoutFragment extends Fragment {
+
+    private SignoutViewModel mViewModel;
+
+    public static SignoutFragment newInstance() {
+        return new SignoutFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_signout, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(SignoutViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Diary4Life/app/src/main/java/com/test/diary4life/Activities/ui/signout/SignoutViewModel.java
@@ -0,0 +1,7 @@
+package com.test.diary4life.Activities.ui.signout;
+
+import androidx.lifecycle.ViewModel;
+
+public class SignoutViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
