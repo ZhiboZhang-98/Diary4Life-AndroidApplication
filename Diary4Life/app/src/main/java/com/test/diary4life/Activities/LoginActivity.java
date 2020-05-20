12498a6b99734f333eeb49b198938d35e1686d07
Zhibo Zhang <61286059+ZhiboZhang-98@users.noreply.github.com>
2020-05-20 18:33:32 -0400

    Finalized LoginActivity Java Class
    Completed LoginActivity Java Class file to adapt the design of its corresponding layout XML file.

--- Diary4Life/app/src/main/java/com/test/diary4life/Activities/LoginActivity.java
+++ Diary4Life/app/src/main/java/com/test/diary4life/Activities/LoginActivity.java
@@ -1,16 +1,124 @@
 package com.test.diary4life.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.test.diary4life.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText userEmail, userPassword;
+    private Button btnLogin;
+    private ProgressBar loginProgress;
+
+    private FirebaseAuth mAuth;
+
+    private Intent HomeActivity;
+
+    private ImageView userProfilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userEmail = findViewById(R.id.txtLoginEmail);
+        userPassword = findViewById(R.id.txtLoginPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        loginProgress = findViewById(R.id.loginProgressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        HomeActivity = new Intent(this, com.test.diary4life.Activities.HomeActivity.class);
+
+        userProfilePicture = findViewById(R.id.imgLoginUserPhoto);
+        userProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerActivity);
+                finish();
+            }
+        });
+
+        loginProgress.setVisibility(View.INVISIBLE);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginProgress.setVisibility(View.VISIBLE);
+                btnLogin.setVisibility(View.INVISIBLE);
+
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if (email.isEmpty() || password.isEmpty())
+                {
+                    showMessage("Please Verify All the Fields, OR Click the User Photo Above to Register As a New User");
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    userSignIn(email, password);
+                }
+            }
+        });
+
     }
+
+    private void userSignIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    btnLogin.setVisibility(View.VISIBLE);
+                    updateUI();
+                }
+                else
+                {
+                    showMessage(task.getException().getMessage());
+                    btnLogin.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+    }
+
+    private void updateUI() {
+        startActivity(HomeActivity);
+        finish();
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null)
+        {
+            // redirect the user to home page if the user is already connected
+            updateUI();
+        }
+    }
+
 }
