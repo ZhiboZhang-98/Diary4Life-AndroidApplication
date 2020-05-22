824497c4bd263c1f0c8510d976cae3822644d919
Zhibo Zhang <61286059+ZhiboZhang-98@users.noreply.github.com>
2020-05-22 16:52:57 -0400

    Finalized ProfileFragment Java Class
    Completed ProfileFragment Java Class file to adapt the design of its corresponding layout XML file. Used Glide Library to load user's profile picture by implementing Glide Library in build.gradle.

--- Diary4Life/app/src/main/java/com/test/diary4life/Activities/ui/profile/ProfileFragment.java
+++ Diary4Life/app/src/main/java/com/test/diary4life/Activities/ui/profile/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,24 +13,38 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.test.diary4life.R;
 
 public class ProfileFragment extends Fragment {
+
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
     private ProfileViewModel profileViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        profileViewModel =
-                ViewModelProviders.of(this).get(ProfileViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        // Initialize the two Firebase Variables
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        TextView UserName = root.findViewById(R.id.txtUserName);
+        TextView UserEmail = root.findViewById(R.id.txtUserEmail);
+        ImageView UserProfilePicture = root.findViewById(R.id.imgUserPhoto);
+
+        UserEmail.setText(currentUser.getEmail());
+        UserName.setText(currentUser.getDisplayName());
+
+        // use Glide to load user's profile picture
+        Glide.with(this).load(currentUser.getPhotoUrl()).into(UserProfilePicture);
+
         return root;
     }
+
 }
