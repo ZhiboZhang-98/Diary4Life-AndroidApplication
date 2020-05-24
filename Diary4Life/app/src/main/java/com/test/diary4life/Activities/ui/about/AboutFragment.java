99377cff3ec04c985d808480ed9d9c9b3466b654
Zhibo Zhang <61286059+ZhiboZhang-98@users.noreply.github.com>
2020-05-24 16:55:20 -0400

    Finalized AboutFragment Java Class
    Completed AboutFragment Java Class file to adapt the design of its corresponding layout XML file. Used one of Android Studio Widgets called WebView to display the application information such as: background, feature, and summary.

--- Diary4Life/app/src/main/java/com/test/diary4life/Activities/ui/about/AboutFragment.java
+++ Diary4Life/app/src/main/java/com/test/diary4life/Activities/ui/about/AboutFragment.java
@@ -11,12 +11,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.test.diary4life.R;
 
 public class AboutFragment extends Fragment {
 
     private AboutViewModel mViewModel;
+
+    private WebView webView;
 
     public static AboutFragment newInstance() {
         return new AboutFragment();
@@ -25,7 +29,17 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+
+        // Inflate the layout for this fragment
+
+        View root = inflater.inflate(R.layout.fragment_about, container, false);
+
+        webView = (WebView)root.findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+
+        return root;
+
     }
 
     @Override
