5daadf8069a0bf431eba8379c2dec70e5fecd515
Zhibo Zhang <61286059+ZhiboZhang-98@users.noreply.github.com>
2020-06-10 19:43:07 -0400

    Finalized HomeFragment Java Class
    Completed HomeFragment Java Class file to adapt the design of fragment_home layout XML file. Initialized Story List collection by binding all data into all the views within this Fragment and created onDataChange function, as well as the onCancelled function.

--- Diary4Life/app/src/main/java/com/test/diary4life/Activities/ui/home/HomeFragment.java
+++ Diary4Life/app/src/main/java/com/test/diary4life/Activities/ui/home/HomeFragment.java
@@ -11,25 +11,87 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.test.diary4life.Models.Story;
 import com.test.diary4life.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    private HomeAddReflectionViewModel homeAddReflectionViewModel;
+    RecyclerView storyRecyclerView;
+    HomeStoryViewModel homeStoryViewModel;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    List<Story> storyList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeAddReflectionViewModel =
-                ViewModelProviders.of(this).get(HomeAddReflectionViewModel.class);
+
+
+
+/*        homePostStoryViewModel =
+               ViewModelProviders.of(this).get(HomePostStoryViewModel.class);*/
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeAddReflectionViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        storyRecyclerView = root.findViewById(R.id.storyRV);
+        storyRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        storyRecyclerView.setHasFixedSize(true);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Stories");
+
+
+/*        final TextView textView = root.findViewById(R.id.text_home);
+       homePostStoryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+           @Override
+           public void onChanged(@Nullable String s) {
+               textView.setText(s);
+           }
+       });*/
+
+        return root;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Get List Stories from the database
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                storyList = new ArrayList<>();
+                for (DataSnapshot postsnap: dataSnapshot.getChildren()) {
+
+                    Story story = postsnap.getValue(Story.class);
+                    storyList.add(story) ;
+
+
+
+                }
+
+                homeStoryViewModel = new HomeStoryViewModel(getActivity(), storyList);
+                storyRecyclerView.setAdapter(homeStoryViewModel);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
-        return root;
+
+
     }
 }
